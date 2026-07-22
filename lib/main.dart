ee827893b0bbdf339c3025d180245f3a75760c2f
khushboo kumari <khusboo707112@gmail.com>
2026-07-22 13:45:41 +0530

    i added register_screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:practice_project/ragister_screen.dart';
+
 void main() {
   runApp(const MyApp());
 }
@@ -9,7 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-
+        home: RegisterScreen(),
     );
   }
 }
