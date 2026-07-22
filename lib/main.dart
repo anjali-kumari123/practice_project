4d0af6b0bde1638e15db5c34076da9fd58ae08fb
Supriya Kumari <supriyasingh2503@gmail.com>
2026-07-22 13:42:38 +0530

    i worked on login screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+
+import 'login_screen.dart';
 void main() {
   runApp(const MyApp());
 }
@@ -9,7 +11,8 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-
+      debugShowCheckedModeBanner: false,
+home: LoginScreen(),
     );
   }
 }
